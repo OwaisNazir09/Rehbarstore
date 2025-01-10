b3001308d9e5e2668fa7eca0d012574166ba044a
[Shagufta Fatima] <shaguftafatima444@gmail.com>
2025-01-10 11:27:55 +0530

    edit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        // primary: "#4263eb",
+        primary:"#FFFFFF",
+        // secondary: "#63e6be",
+        secondary: "#FFFFFF",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "3rem",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
